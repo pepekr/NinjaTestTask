7c55c83e892ef2afbbc50dc8f0d25d2f474603bd
illia <illiakrang@gmail.com>
2025-08-29 13:17:55 +0300

    feat: add super hero interface

---
+++ shared/interfaces/SuperHero.ts
@@ -0,0 +1,9 @@
+// no need for any dtos no confidential or unneccessary info 
+export interface SuperHeroWithoutImages {
+    id: string;
+    nickname: string;
+    real_name: string;
+    origin_description: string;
+    superpowers: string;
+    catch_phrase: string;
+}
