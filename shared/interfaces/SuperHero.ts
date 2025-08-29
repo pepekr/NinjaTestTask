2d201b0e8fe6a181185783a6e490ad8ef6137f92
illia <illiakrang@gmail.com>
2025-08-29 15:03:01 +0300

    feat: add creational interface

--- shared/interfaces/SuperHero.ts
+++ shared/interfaces/SuperHero.ts
@@ -1,6 +1,17 @@
+import { HeroImage } from "./HeroImage";
+
 // no need for any dtos no confidential or unneccessary info 
 export interface Superhero {
     id: string;
+    nickname: string;
+    real_name: string;
+    origin_description: string;
+    superpowers: string;
+    catch_phrase: string;
+}
+
+export interface SuperheroCreational
+{
     nickname: string;
     real_name: string;
     origin_description: string;
