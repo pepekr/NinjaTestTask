9f5b84de1a58e4da458b5a02cd5ec8badd43c2b3
illia <illiakrang@gmail.com>
2025-08-30 16:37:28 +0300

    feat: basic setup and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./index.css";
 function App() {
- 
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="min-h-screen w-full bg-gray-50">
+      <Navbar />
+
+      <main className="p-6 w-full">
+        <Routes>
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
