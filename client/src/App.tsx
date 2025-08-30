86dafb77f9e04b4d8c5d4436192b80f26357bea9
illia <illiakrang@gmail.com>
2025-08-30 17:26:29 +0300

    feat: WORKING form before hooks creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./index.css";
+import CreateHero from "./pages/CreateHero";
 function App() {
   return (
     <div className="min-h-screen w-full bg-gray-50">
@@ -8,6 +9,7 @@ function App() {
 
       <main className="p-6 w-full">
         <Routes>
+          <Route path="/create-hero" element = {<CreateHero/>}/>
         </Routes>
       </main>
     </div>
