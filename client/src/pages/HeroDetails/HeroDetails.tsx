e9f58f1dce28c67c470e357a908aa98961a94031
illia <illiakrang@gmail.com>
2025-08-31 10:45:31 +0300

    feat: adapt jsx logic for adding image

--- client/src/pages/HeroDetails/HeroDetails.tsx
+++ client/src/pages/HeroDetails/HeroDetails.tsx
@@ -8,9 +8,6 @@ function handleDeleteHero(heroId:string) {
   // TODO: delete hero by id
 }
 
-function handleAddImage(heroId:string) {
-  // TODO: add new picture for hero
-}
 
 function handleEditHero(id:string) {
   // TODO: edit hero details
@@ -19,7 +16,7 @@ function handleEditHero(id:string) {
 export default function HeroDetails() {
   const location = useLocation();
   const hero = location.state as Superhero;
-  const { heroImages, getImages } = useHeroImages();
+  const { heroImages, getImages, handleAddImages } = useHeroImages();
 
   useEffect(() => {
     if (hero) getImages(hero.id);
@@ -27,8 +24,10 @@ export default function HeroDetails() {
 
   if (!hero) return <div className="text-center mt-10">Hero not found</div>;
 
+
   return (
     <div className="max-w-3xl mx-auto p-4 md:p-8">
+        <div>
       <h1 className="text-4xl font-bold mb-6 text-center text-indigo-600">
         {hero.nickname}
       </h1>
@@ -54,7 +53,7 @@ export default function HeroDetails() {
         </p>
         <p className="text-lg italic text-gray-700">"{hero.catch_phrase}"</p>
       </div>
-
+</div>
       {/* Buttons */}
       <div className="flex justify-between mt-6 gap-4">
         <button
@@ -62,12 +61,6 @@ export default function HeroDetails() {
           className="px-4 py-2 bg-red-500 text-black rounded-lg hover:bg-red-600"
         >
           Delete Hero
-        </button>
-        <button
-          onClick={()=>handleAddImage}
-          className="px-4 py-2 bg-green-500 text-black rounded-lg hover:bg-green-600"
-        >
-          Add New Picture
         </button>
         <button
           onClick={()=>handleEditHero}
