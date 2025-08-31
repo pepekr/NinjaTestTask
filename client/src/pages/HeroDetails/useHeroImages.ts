9aee32d7efbbf216672ab4824d88bb9313db657e
illia <illiakrang@gmail.com>
2025-08-31 10:31:08 +0300

    feat: add logic for adding new images

--- client/src/pages/HeroDetails/useHeroImages.ts
+++ client/src/pages/HeroDetails/useHeroImages.ts
@@ -11,13 +11,30 @@ export function useHeroImages() {
       );
 
       if (!response.ok) {
-       
       } // make component or just jsx logic
       else {
-        const {heroImages} = await response.json();
+        const { heroImages } = await response.json();
         setImages(heroImages);
       }
     } catch (error) {}
   }
-  return { heroImages, getImages };
+  async function handleAddImages(images: File[], heroId: string) {
+    if (images.length > 0) {
+      const formData = new FormData();
+      images.forEach((file) => formData.append("images", file));
+      const imageResponse = await fetch(
+        `${import.meta.env.VITE_BACKEND_API_URL}/images/addImages/${heroId}`,
+        { method: "PUT", body: formData }
+      );
+
+      if (!imageResponse.ok) {
+        const err = await imageResponse.json();
+        alert(err);
+      } else {
+        const newImages: HeroImage[] = await imageResponse.json();
+        setImages((prev) => [...prev, ...newImages]);
+      }
+    }
+  }
+  return { heroImages, getImages, handleAddImages };
 }
