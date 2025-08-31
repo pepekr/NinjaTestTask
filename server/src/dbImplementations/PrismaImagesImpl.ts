483f63f72f11a077b450d42518f69243b5cfac26
illia <illiakrang@gmail.com>
2025-08-31 14:58:08 +0300

    feat: add order in implementations

--- server/src/dbImplementations/PrismaImagesImpl.ts
+++ server/src/dbImplementations/PrismaImagesImpl.ts
@@ -8,7 +8,10 @@ import { IGetByHeroId } from "../../../shared/interfaces/IGetByHeroId.js";
  * Prisma implementation of db operations
  */
 export default class PrismaImagesImpl
-  implements ICrudRepository<HeroImage>, IGetRepository<HeroImage>, IGetByHeroId
+  implements
+    ICrudRepository<HeroImage>,
+    IGetRepository<HeroImage>,
+    IGetByHeroId
 {
   async create(item: HeroImage): Promise<HeroImage> {
     return prismaClient.image.create({ data: item });
@@ -17,7 +20,11 @@ export default class PrismaImagesImpl
     return prismaClient.image.findUnique({ where: { id: itemId } });
   }
   async getAll(offset?: number, take?: number): Promise<HeroImage[]> {
-    return prismaClient.image.findMany({ skip: offset, take: take });
+    return prismaClient.image.findMany({
+      skip: offset,
+      take: take,
+      orderBy: { createdAt: "desc" },
+    });
   }
   async delete(itemId: string): Promise<HeroImage> {
     return prismaClient.image.delete({ where: { id: itemId } });
@@ -30,12 +37,16 @@ export default class PrismaImagesImpl
   }
 
   /**
-   * 
+   *
    * @param heroId The id of a hero object whose images we want retreive
    * @param offset The amount of skipped images
    * @param take The amount of taken images
    */
-  async getByHeroId(heroId: string, offset?: number, take?: number):Promise<HeroImage[]> {
+  async getByHeroId(
+    heroId: string,
+    offset?: number,
+    take?: number
+  ): Promise<HeroImage[]> {
     return prismaClient.image.findMany({
       where: { imageOwnerId: heroId },
       skip: offset,
--- server/src/dbImplementations/PrismaSuperheroImpl.ts
+++ server/src/dbImplementations/PrismaSuperheroImpl.ts
@@ -5,21 +5,32 @@ import prismaClient from "prismaClient.js";
 /**
  * Prisma implementation of db operations
  */
-export default class PrismaSuperheroImpl implements ICrudRepository<Superhero>, IGetRepository<Superhero>
+export default class PrismaSuperheroImpl
+  implements ICrudRepository<Superhero>, IGetRepository<Superhero>
 {
-   async create(item: Superhero): Promise<Superhero> {
-       return prismaClient.superHero.create({data:item});
-   }
-   async getById(itemId: string): Promise<Superhero|null> {
-       return prismaClient.superHero.findUnique({where:{id:itemId}});
-   }
-   async getAll(offset?: number, take?:number): Promise<Superhero[]> {
-       return prismaClient.superHero.findMany({skip: offset, take: take});
-   }
-   async delete(itemId: string): Promise<Superhero> {
-       return prismaClient.superHero.delete({where:{id:itemId}})
-   }
-   async update(itemId: string, itemPart: Partial<Superhero>): Promise<Superhero> {
-       return prismaClient.superHero.update({where:{id:itemId}, data:itemPart})
-   }
+  async create(item: Superhero): Promise<Superhero> {
+    return prismaClient.superHero.create({ data: item });
+  }
+  async getById(itemId: string): Promise<Superhero | null> {
+    return prismaClient.superHero.findUnique({ where: { id: itemId } });
+  }
+  async getAll(offset?: number, take?: number): Promise<Superhero[]> {
+    return prismaClient.superHero.findMany({
+      skip: offset,
+      take: take,
+      orderBy: { createdAt: "desc" },
+    });
+  }
+  async delete(itemId: string): Promise<Superhero> {
+    return prismaClient.superHero.delete({ where: { id: itemId } });
+  }
+  async update(
+    itemId: string,
+    itemPart: Partial<Superhero>
+  ): Promise<Superhero> {
+    return prismaClient.superHero.update({
+      where: { id: itemId },
+      data: itemPart,
+    });
+  }
 }
